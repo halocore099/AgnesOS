b6b64dcf559746415a61d5c03038041b312f1112
Navitank <halocore099@gmail.com>
2026-07-20 18:18:41 +0200

    Update updater.lua

--- system/updater.lua
+++ system/updater.lua
@@ -1,86 +1,48 @@
 local config = require("config")
 
-
 local function getRemoteVersion()
-
-    local url =
-    config.UPDATE_URL .. "version.txt"
-
+    local url = config.UPDATE_URL .. "version.txt"
     local response = http.get(url)
-
     if not response then
         return nil
     end
-
     local version = response.readAll()
-
     response.close()
-
     return version
-
 end
-
-
 
 local function updateFile(file)
-
-    print("Updating "..file)
-
-    local url =
-    config.UPDATE_URL .. file
-
-
-    shell.run(
-        "wget",
-        url,
-        file
-    )
-
+    print("Updating " .. file)
+    local url = config.UPDATE_URL .. file
+    shell.run("wget", url, file)
 end
-
-
 
 print("Checking updates...")
 
-
 local remote = getRemoteVersion()
 
-
 if not remote then
-
     print("Offline mode")
     return
-
 end
 
-
-
 if remote ~= config.VERSION then
-
     print("")
     print("Update found!")
-    print(
-        config.VERSION ..
-        " -> " ..
-        remote
-    )
+    print(config.VERSION .. " -> " .. remote)
 
     sleep(2)
-
 
     updateFile("AgnesOS.lua")
     updateFile("config.lua")
     updateFile("version.txt")
-
+    updateFile("startup.lua")
+    updateFile("system/ui.lua")
 
     print("")
     print("Updated!")
-
 else
-
     print("AgnesOS is up to date")
-
 end
-
 
 sleep(1)
